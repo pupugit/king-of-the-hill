07f05f4be6e7dff24d3bbf3f49e8771951906d87
Ferydun Amati <office@amati.solutions>
2022-12-16 00:55:42 +0100

    add note about timezone

--- components/BlockWeek.vue
+++ components/BlockWeek.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="block-week">
-    <h2>Week {{ week.nr }}: {{ startedF }}</h2>
+    <h2>Week {{ week.nr }}: {{ startedF }}<br /><span style="font-weight: normal;font-size: .75em;">date/times are
+        in your local time</span></h2>
     <div :class="`matches size-${matches.length}`">
       <BlockMatch v-for="(match, idx) in matches" :match="match" :key="match.id" :class="`match match-${idx}`" />
     </div>
